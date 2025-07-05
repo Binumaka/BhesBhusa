8f604d07bab94c358b3f740f8ec021924eaee603
Binu Maka <binumaka9@gmail.com>
2025-07-05 18:21:25 +0545

    auth js added

---
+++ BhesBhusa_server/security/Auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Access denied: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
+
+const authorizeRole = (role) => (req, res, next) => {
+  if (req.user.role !== role) {
+    return res
+      .status(403)
+      .json({ error: "Access denied: Insufficient permissions" });
+  }
+
+  next();
+};
+
+module.exports = {
+  authenticateToken,
+  authorizeRole,
+};
