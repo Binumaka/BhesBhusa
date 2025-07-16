63105ebb395f5ab1e128a062f8b6ee28a803a312
Binu Maka <binumaka9@gmail.com>
2025-07-16 18:46:48 +0545

    shopping cart routes

---
+++ BhesBhusa_server/routes/shoppingCartRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+    addToCart,
+    getCartById,
+    removeFromCart
+} = require('../controller/shoppingCartController');
+const {authenticateToken} = require("../security/Auth")
+
+const router = express.Router();
+
+router.post('/save', addToCart);
+router.get('/:userId', getCartById);
+router.delete('/:userId/:clothId', authenticateToken, removeFromCart);
+
+module.exports = router;
