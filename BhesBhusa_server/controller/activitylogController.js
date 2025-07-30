b314b681df47397e1e3ac12f25702d1f96191e9b
Binu Maka <binumaka9@gmail.com>
2025-07-30 22:17:08 +0545

    added activity log controller

---
+++ BhesBhusa_server/controller/activitylogController.js
@@ -0,0 +1,19 @@
+const ActivityLog = require("../model/activitylogModel");
+
+const getActivityLogs = async (req, res) => {
+  try {
+    const currentUserId = req.user?._id;
+
+    const logs = await ActivityLog.find({
+      userId: { $ne: currentUserId }, // Exclude current admin
+    })
+      .populate("userId", "username email")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({ success: true, logs });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Failed to fetch logs" });
+  }
+};
+
+module.exports = { getActivityLogs };
